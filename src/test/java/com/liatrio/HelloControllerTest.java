3cd65fec2ff62ae5faee357c048233b39951fe68
baserrato <benjamins@liatrio.com>
2024-04-17 12:18:36 -0700

    Add unit test for HelloController

--- src/test/java/com/liatrio/HelloControllerTest.java
+++ src/test/java/com/liatrio/HelloControllerTest.java
@@ -1,5 +1,23 @@
 package com.liatrio;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HelloControllerTest {
-  // get hello response
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getHelloResponseTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("Hello, World!"));
+    }
 }
